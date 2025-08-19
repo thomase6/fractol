27f4ef42d7241c0b06a6401ca79053ab12c4fa93
root <root@LittleToaster.localdomain>
2025-08-19 19:32:45 +0200

    added a single red pixel

--- src/fractol.h
+++ src/fractol.h
@@ -13,10 +13,21 @@
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
+// window size
 # define WIDTH 1920
 # define HEIGHT 1080
 
+// libraries
 #include <mlx.h>
+
+typedef struct s_data
+{
+    void    *img;
+    char    *addr;
+    int     bpp;
+    int     line_length;
+    int     endian; 
+}           t_data;
 
 int main(void);
 
--- src/window.c
+++ src/window.c
@@ -12,12 +12,25 @@
 
 #include "fractol.h"
 
+void my_mlx_put_pixel(t_data *data, int x, int y, int color)
+{
+    char *dst;
+
+    dst = data->addr + (y * data->line_length + x * (data->bpp / 8));
+    *(unsigned int*)dst = color;
+}
+
 int main(void)
 {
     void *mlx;
     void *mlx_win;
+    t_data  img;  
 
     mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, WIDTH, HEIGHT, "Hello world!");
+    mlx_win = mlx_new_window(mlx, WIDTH, HEIGHT, "The Void");
+    img.img = mlx_new_image(mlx, WIDTH, HEIGHT);
+    img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.line_length, &img.endian);
+    my_mlx_put_pixel(&img, 5, 5, 0x00FF0000);
+    mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
     mlx_loop(mlx);
 }
